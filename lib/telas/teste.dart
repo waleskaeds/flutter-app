f495d20acfbda310950ceb12ea7439e9202ad5c8
waleskaeds <waleskaeds05@gmail.com>
2023-03-11 11:21:27 -0300

    add teste

---
+++ lib/telas/teste.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Teste extends StatefulWidget {
+  const Teste({super.key});
+
+  @override
+  State<Teste> createState() => _TesteState();
+}
+
+class _TesteState extends State<Teste> {
+  @override
+  Widget build(BuildContext context) {
+    return const Teste();
+  }
+}
